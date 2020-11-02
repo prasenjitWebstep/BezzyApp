fa3731ba43f7dc55e47911427c2c1e3417a0fcfc
WT136 <sujoy.saha@webstep.in>
2020-11-02 12:04:22 +0530

    Useless branch just for testing should not be merged

--- Easy_Application/app/src/main/java/com/bezzy/Ui/View/activity/Fragments/Photo_fragment.java
+++ Easy_Application/app/src/main/java/com/bezzy/Ui/View/activity/Fragments/Photo_fragment.java
@@ -31,9 +31,11 @@ import android.widget.Toast;
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -41,6 +43,7 @@ import com.bezzy.Ui.View.activity.Profile;
 import com.bezzy.Ui.View.utils.APIs;
 import com.bezzy.Ui.View.utils.Utility;
 import com.bezzy.Ui.View.utils.VolleyMultipartRequest;
+import com.bezzy.Ui.View.utils.VolleyMultipartRequestImage;
 import com.bezzy_application.R;
 import com.google.android.material.textfield.TextInputLayout;
 import com.theartofdev.edmodo.cropper.CropImage;
@@ -52,6 +55,7 @@ import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -106,17 +110,7 @@ public class Photo_fragment extends Fragment {
                 ((Activity) getActivity()).overridePendingTransition(0, 0);
             }
         });
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
-//                    String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-//                    requestPermissions(permissions,PERMISSION_CODE);
-//                }else{
-//                    pickImageFromGallery();
-//                }
-//            }
-//        });
+
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -188,46 +182,13 @@ public class Photo_fragment extends Fragment {
                 }
             } else if (data.getData() != null) {
                 resultUri = data.getData();
-                //resultUri = data.getData();
                 filePath = getPath(resultUri);
                 uploadBitmap(bitmap,APIs.BASE_URL+ APIs.POSTIMAGE);
                 // display your image
                 imageView.setImageURI(resultUri);
             }
         }
-            //imageView.setImageURI(data.getData());
-//            resultUri = data.getData();
-//            try{
-//                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),resultUri);
-//                imageView.setImageBitmap(bitmap);
-//                //uploadImage(bitmap, APIs.BASE_URL+APIs.POSTIMAGE);
-//
-//            } catch (FileNotFoundException e) {
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-       // }
-//        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-//            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-//            if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-//                imageView.setImageURI(data.getData());
-//                resultUri = data.getData();
-//                try {
-//                    bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), resultUri);
-//
-//                        uploadImage(bitmap, APIs.BASE_URL+APIs.POSTIMAGE);
-//
-//                    //imageView.setImageBitmap(bitmap);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                    Log.e("Exception",e.toString());
-//                }
-//            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-//                Exception error = result.getError();
-//                Log.e("ExceptionError",error.toString());
-//            }
-//        }
+
     }
 
     public String getPath(Uri uri) {
@@ -256,102 +217,6 @@ public class Photo_fragment extends Fragment {
 
     private void uploadBitmap(final Bitmap bitmap,String url) {
 
-        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, url,
-                new Response.Listener<NetworkResponse>() {
-                    @Override
-                    public void onResponse(NetworkResponse response) {
-                        try {
-                            JSONObject obj = new JSONObject(new String(response.data));
-                            Toast.makeText(getContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-                        Log.e("GotError",""+error.getMessage());
-                    }
-                }) {
-
-
-            @Override
-            protected Map<String, VolleyMultipartRequest.DataPart> getByteData() {
-                Map<String, DataPart> params = new HashMap<>();
-                long imagename = System.currentTimeMillis();
-                params.put("image", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
-                return params;
-            }
-        };
-
-        //adding the request to volley
-        Volley.newRequestQueue(getContext()).add(volleyMultipartRequest);
     }
-//    private void uploadImage(final Bitmap bitmap, String url, final String caption_upload) {
-//        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//
-//                Log.e("Response",response);
-//                try {
-//                    JSONObject object = new JSONObject(response);
-//                    String resp = object.getString("resp");
-//                    if(resp.equals("success")){
-//                        progressDialog.dismiss();
-//                        Toast.makeText(getActivity(),object.getString("message"),Toast.LENGTH_LONG).show();
-//                        Intent intent = new Intent(getActivity(),Profile.class);
-//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                        startActivity(intent);
-//
-//                    }else{
-//                        progressDialog.dismiss();
-//                        Toast.makeText(getActivity(),object.getString("message"),Toast.LENGTH_SHORT).show();
-//                    }
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//
-//                    progressDialog.dismiss();
-//                    Log.e("Exception",e.toString());
-//                }
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//
-//                progressDialog.dismiss();
-//                Log.e("Error",error.toString());
-//            }
-//        }){
-//            @Override
-//            protected Map<String, String> getParams() throws AuthFailureError {
-//                HashMap<String,String> map = new HashMap<>();
-//                map.put("userID", Utility.getUserId(getActivity()));
-////                map.put("post_image",getEncoded64ImageStringFromBitmap(bitmap));
-//                map.put("post_content",caption_upload);
-//                //Log.e("image",map.get("profile_picture"));
-//                return map;
-//            }
-//        };
-//
-//        RequestQueue queue = Volley.newRequestQueue(getActivity());
-//        queue.add(request);
-//        request.setRetryPolicy(new DefaultRetryPolicy(
-//                MY_SOCKET_TIMEOUT_MS,
-//                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-//                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-//
-//    }
 
-//    public String getEncoded64ImageStringFromBitmap(Bitmap bitmap) {
-//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//        bitmap.compress(Bitmap.CompressFormat.JPEG, 25, stream);
-//        byte[] byteFormat = stream.toByteArray();
-//        // get the base 64 string
-//        String imgString = Base64.encodeToString(byteFormat, Base64.NO_WRAP);
-//        base64String= imgString;
-//        //Log.d("hello",imgString);
-//        return imgString;
-//    }
 }
