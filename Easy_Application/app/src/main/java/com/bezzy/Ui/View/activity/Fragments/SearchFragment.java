396ece74fc507ba496d2234ea243675d0e6e7b1b
WT137 <prasenjit.saha@webstep.in>
2020-11-06 17:17:06 +0530

    search api integration

--- Easy_Application/app/src/main/java/com/bezzy/Ui/View/activity/Fragments/SearchFragment.java
+++ Easy_Application/app/src/main/java/com/bezzy/Ui/View/activity/Fragments/SearchFragment.java
@@ -3,6 +3,7 @@ package com.bezzy.Ui.View.activity.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
@@ -14,13 +15,18 @@ import android.widget.SearchView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bezzy.Ui.View.adapter.SearchAdapter;
+import com.bezzy.Ui.View.adapter.Search_adapter;
+import com.bezzy.Ui.View.model.Friendsnoti_item;
 import com.bezzy.Ui.View.utils.APIs;
 import com.bezzy_application.R;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,7 +40,9 @@ public class SearchFragment extends Fragment {
     SearchView searchView;
     Button button;
     //RecyclerView recyclerView;
-    ArrayList<String> nameList;
+    ArrayList<Friendsnoti_item> dataholder;
+    RecyclerView recyclerViewSearchResult;
+    Friendsnoti_item ob1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,14 +51,11 @@ public class SearchFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         //recyclerView = view.findViewById(R.id.recyclerView);
         searchView = view.findViewById(R.id.searchView);
-        button = view.findViewById(R.id.www);
+        recyclerViewSearchResult = view.findViewById(R.id.recyclerViewSearchResult);
+        dataholder = new ArrayList<>();
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                search(APIs.BASE_URL+APIs.SEARCH);
-            }
-        });
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerViewSearchResult.setLayoutManager(linearLayoutManager);
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -68,15 +73,25 @@ public class SearchFragment extends Fragment {
     }
 
     private void search(String url){
+        dataholder.clear();
         StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                Log.e("Response",response);
                 try {
                     JSONObject object = new JSONObject(response);
-                    String data = object.getString("name");
-                    nameList.add(data);
-                    Log.e("ASDF",nameList.toString());
-                    //recyclerView.setAdapter(new SearchAdapter(getActivity(),nameList));
+                    String resp = object.getString("resp");
+                    if(resp.equals("success")){
+                        JSONArray array = object.getJSONArray("search_result");
+                        for(int i = 0;i<array.length();i++){
+                            JSONObject object1 = array.getJSONObject(i);
+
+                            ob1 = new Friendsnoti_item(object1.getString("name"),object1.getString("user_bio"),object1.getString("image"),object1.getString("user_id"));
+                            dataholder.add(ob1);
+
+                        }
+                    }
+                    recyclerViewSearchResult.setAdapter(new Search_adapter(dataholder,getActivity()));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -90,10 +105,14 @@ public class SearchFragment extends Fragment {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 HashMap<String,String> map = new HashMap<>();
-                map.put("searchval","suj");
+                map.put("searchval",searchView.getQuery().toString());
+                Log.e("Value",map.get("searchval"));
                 //searchView.getQuery().toString()
                 return map;
             }
         };
+
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        queue.add(request);
     }
 }
