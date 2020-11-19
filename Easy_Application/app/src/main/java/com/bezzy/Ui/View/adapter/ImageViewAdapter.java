eeb96eb653353e111263c18665f1ecc7046652d5
Shoumik polman <shoumik07.polman@gmail.com>
2020-11-19 15:23:50 +0530

    chat box ui and funtionalityhh

--- Easy_Application/app/src/main/java/com/bezzy/Ui/View/adapter/ImageViewAdapter.java
+++ Easy_Application/app/src/main/java/com/bezzy/Ui/View/adapter/ImageViewAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -46,6 +47,7 @@ public class ImageViewAdapter extends RecyclerView.Adapter<ImageViewAdapter.Imag
     }
 
     public class ImageHolder extends RecyclerView.ViewHolder{
+        TextView tvtime;
 
         ImageView imageDisplay;
 
