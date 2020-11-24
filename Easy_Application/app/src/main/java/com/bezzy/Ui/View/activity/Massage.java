6dbe14f1ea0316fa7ffbeccda4e03daf6599ff55
WT137 <prasenjit.saha@webstep.in>
2020-11-24 11:00:11 +0530

    Merge Branch 13.0

--- Easy_Application/app/src/main/java/com/bezzy/Ui/View/activity/Massage.java
+++ Easy_Application/app/src/main/java/com/bezzy/Ui/View/activity/Massage.java
@@ -49,7 +49,7 @@ public class Massage extends AppCompatActivity {
     ArrayList<ChatMessageModel> modelArrayList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_massage);
 
