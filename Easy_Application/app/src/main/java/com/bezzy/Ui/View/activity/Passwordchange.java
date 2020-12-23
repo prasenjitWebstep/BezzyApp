48454bb8455f6f9d4f81cdbeee2ddc16a560f452
Shoumik polman <shoumik07.polman@gmail.com>
2020-12-23 18:31:56 +0530

    change pass xml

--- Easy_Application/app/src/main/java/com/bezzy/Ui/View/activity/Passwordchange.java
+++ Easy_Application/app/src/main/java/com/bezzy/Ui/View/activity/Passwordchange.java
@@ -2,16 +2,31 @@ package com.bezzy.Ui.View.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bezzy.Ui.View.utils.APIs;
 import com.bezzy.Ui.View.utils.Utility;
 import com.bezzy_application.R;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import dmax.dialog.SpotsDialog;
 
@@ -20,6 +35,7 @@ public class Passwordchange extends AppCompatActivity {
     Button change;
     SpotsDialog progressDialog;
     String userId;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +45,12 @@ public class Passwordchange extends AppCompatActivity {
         password = findViewById(R.id.password);
         cnf_password = findViewById(R.id.cnf_password);
         change = findViewById(R.id.change);
-        new_password=findViewById(R.id.new_password);
+        progressBar =findViewById(R.id.progressBar);
+        //new_password=findViewById(R.id.new_password);
         /*progressDialog = new SpotsDialog(Changepassword.this);
         progressDialog.setCancelable(false);
         progressDialog.setMessage("Loading Please Wait..");*/
-        //userId = getIntent().getExtras().getString("USERID");
+        userId =Utility.getUserId(Passwordchange.this);
 
         change.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,17 +66,83 @@ public class Passwordchange extends AppCompatActivity {
                     Log.e("Called","1");
                     if(Utility.internet_check(Passwordchange.this)) {
                         //progressDialog.show();
-                        Utility.displayLoader(Passwordchange.this);
-                        //callAPIChangePassword(APIs.BASE_URL+APIs.CHANGEPASSWORD);
+                        //Utility.displayLoader(Passwordchange.this);
+                        progressBar.setVisibility(View.VISIBLE);
+                        change.setVisibility(View.GONE);
+                        callAPIChangePassword(APIs.BASE_URL+APIs.CHANGEPASSWORD);
                     }
                     else {
                         //progressDialog.dismiss();
-                        Utility.hideLoader(Passwordchange.this);
+                        //Utility.hideLoader(Passwordchange.this);
+                        progressBar.setVisibility(View.GONE);
+                        change.setVisibility(View.VISIBLE);
                         Toast.makeText(Passwordchange.this,"No Network!",Toast.LENGTH_SHORT).show();
                     }
                 }
 
             }
         });
+    }
+    private void callAPIChangePassword(String url){
+        Log.e("URL",url);
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.e("Response",response);
+                try {
+                    JSONObject object = new JSONObject(response);
+                    String resp = object.getString("resp");
+                    if(resp.equals("true")){
+                        //progressDialog.dismiss();
+                        //Utility.hideLoader(Passwordchange.this);
+                        progressBar.setVisibility(View.GONE);
+                        change.setVisibility(View.VISIBLE);
+                        Toast.makeText(Passwordchange.this,object.getString("reg_msg"),Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Passwordchange.this,LoginActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }else{
+                        //progressDialog.dismiss();
+                        //Utility.hideLoader(Passwordchange.this);
+                        progressBar.setVisibility(View.GONE);
+                        change.setVisibility(View.VISIBLE);
+                        Toast.makeText(Passwordchange.this,object.getString("reg_msg"),Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //progressDialog.dismiss();
+                    //Utility.hideLoader(Passwordchange.this);
+                    progressBar.setVisibility(View.GONE);
+                    change.setVisibility(View.VISIBLE);
+                    Log.e("Exception",e.toString());
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //progressDialog.dismiss();
+                //Utility.hideLoader(Passwordchange.this);
+                progressBar.setVisibility(View.GONE);
+                change.setVisibility(View.VISIBLE);
+                Log.e("Error",error.toString());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> map = new HashMap<>();
+                map.put("userID",userId);
+                map.put("password",cnf_password.getText().toString());
+                Log.e("MAPGET",map.get("password")+" "+map.get("userID"));
+                return map;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(Passwordchange.this);
+        queue.add(request);
+
     }
 }
