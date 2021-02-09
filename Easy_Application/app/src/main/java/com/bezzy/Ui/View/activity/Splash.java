d563845b461cc619b3a655c3b2b76d4283f57e5a
WT137 <prasenjit.saha@webstep.in>
2021-02-09 16:46:20 +0530

    Splash logo

--- Easy_Application/app/src/main/java/com/bezzy/Ui/View/activity/Splash.java
+++ Easy_Application/app/src/main/java/com/bezzy/Ui/View/activity/Splash.java
@@ -2,6 +2,7 @@ package com.bezzy.Ui.View.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -15,6 +16,10 @@ import android.widget.TextView;
 
 import com.bezzy.Ui.View.utils.Utility;
 import com.bezzy_application.R;
+import com.bumptech.glide.GenericTransitionOptions;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.transition.ViewPropertyTransition;
 
 public class Splash extends AppCompatActivity {
     private View decorView;
@@ -30,6 +35,22 @@ public class Splash extends AppCompatActivity {
         button = findViewById(R.id.splash_button);
         logo=(ImageView)findViewById(R.id.logo_bezzy);
 
+        ViewPropertyTransition.Animator animationObject = new ViewPropertyTransition.Animator() {
+            @Override
+            public void animate(View view) {
+                view.setAlpha(0f);
+
+                ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f);
+                fadeAnim.setDuration(2500);
+                fadeAnim.start();
+            }
+        };
+
+        Glide.with(Splash.this)
+                .load(R.drawable.logo)
+                .transition(GenericTransitionOptions.with(animationObject))
+                .into(logo);
+
         Log.e("GETLOGIN",Utility.getLogin(Splash.this));
 
         if(Utility.getLogin(Splash.this).equals("1")){
@@ -43,8 +64,6 @@ public class Splash extends AppCompatActivity {
 
                 }
             },splashtimeout);
-            Animation mysin= AnimationUtils.loadAnimation(this,R.anim.animation);
-            logo.startAnimation(mysin);
         }else{
             button.setVisibility(View.VISIBLE);
             button.setOnClickListener(new View.OnClickListener() {
